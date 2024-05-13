00bc20d7297e3a990861271eed0e9ba1445ae07a
Pedro Oliveira <97547565+pedrojcoliveira@users.noreply.github.com>
2024-05-13 14:11:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/asfirstapp/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.asfirstapp
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btnAdd : Button
+    lateinit var btnSub : Button
+    lateinit var btnMul : Button
+    lateinit var btnDiv : Button
+    lateinit var etA : EditText
+    lateinit var etB : EditText
+    lateinit var resultTv : TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        btnAdd = findViewById(R.id.btn_add)
+        btnSub = findViewById(R.id.btn_subtraction)
+        btnMul = findViewById(R.id.btn_multiplication)
+        btnDiv = findViewById(R.id.btn_division)
+        etA = findViewById(R.id.et_a)
+        etB = findViewById(R.id.et_b)
+        resultTv = findViewById(R.id.result_tv)
+
+        btnAdd.setOnClickListener(this)
+        btnSub.setOnClickListener(this)
+        btnMul.setOnClickListener(this)
+        btnDiv.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        var a = etA.text.toString().toDouble()
+        var b = etB.text.toString().toDouble()
+        var result = 0.0
+        when(v?.id){
+            R.id.btn_add-> {
+                result = a+b
+            }
+            R.id.btn_subtraction-> {
+                result = a-b
+            }
+            R.id.btn_multiplication-> {
+                result = a*b
+            }
+            R.id.btn_division-> {
+                result = a/b
+            }
+        }
+        resultTv.text = "Result is $result"
+    }
+}
